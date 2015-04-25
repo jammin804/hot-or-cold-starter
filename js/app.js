e80c8c659769b6ba90396e0e8deb4a2cedd276fc
jammin804 <jammin804@gmail.com>
2015-04-25 10:21:23 -0400

    Same commit..Forgot to save

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var secretNumber;
 $(document).ready(function(){
+
     /*--- On load start a new game ---*/
     newGame();
     console.log(secretNumber);
@@ -7,6 +8,7 @@ $(document).ready(function(){
         newGame();
         console.log(secretNumber);
     });
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -18,9 +20,37 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    /*--- Evaluates the user answer and responses with hot or cold ---*/
+    $("#guessButton").click(function(){
+
+        }
+    });
+
 });
 
-    function newGame(){
-        secretNumber = Math.floor(Math.random() * 100) + 1;
-        return secretNumber;
+function newGame(){
+    secretNumber = Math.floor(Math.random() * 100) + 1;
+    return secretNumber;
+}
+
+function hotAndCold(number){
+    distanceBtwn = Math.abs(secretNumber - number);
+    if(distanceBtwn > 50 || distanceBtwn > secretNumber){
+        console.log('Ice Cold');
     }
+    else if(distanceBtwn > 30 && distanceBtwn < 50){
+        console.log('Cold');
+    }
+    else if(distanceBtwn > 20 && distanceBtwn < 30){
+        console.log('Warm');
+    }
+    else if(distanceBtwn > 10 && distanceBtwn < 20){
+        console.log('Hot');
+    }
+    else if(distanceBtwn > 1 && distanceBtwn < 10){
+        console.log('Very Hot');
+    }
+    else{
+        console.log('They match!')
+    }
+}
